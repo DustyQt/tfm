c713196de8e6c2afeec541c09901cd58bff783a9
DustyQt <DustyQt>
2025-01-26 20:03:06 +0000

    add silver pipes

---
+++ pipelines/silver_murder_turkey.py
@@ -0,0 +1,31 @@
+import pyspark.sql.functions as F
+import os
+
+STORAGE_ACCOUNT = os.getenv('STORAGE_ACCOUNT')
+STORAGE_ACCOUNT_KEY = os.getenv('STORAGE_ACCOUNT_KEY')
+
+@dlt.table(name='turkey_murder', comment='turkey murder data')
+def silver_turkey_murder():
+    spark.conf.set(STORAGE_ACCOUNT, STORAGE_ACCOUNT_KEY)
+    turkey_df = spark.read\
+                    .option("header", "true")\
+                    .option("inferSchema", "true")\
+                    .csv("abfss://datasets@tfmstorageacc.dfs.core.windows.net/violence_against_women_turkey.csv")
+
+    return turkey_df.withColumn(
+        'year',
+        F.year(F.to_date(F.col('Date'), 'dd/MM/yyyy')),
+    ).withColumn(
+        'country', F.lit('turkey'),
+    ).withColumn(
+        'age_group', F.lit('any'),
+    ).where(
+        F.col('year').isNotNull(),
+    ).select(
+        F.col('age_group'),
+        F.col('year'),
+        F.col('country'),
+        F.col('Province').alias('province'),
+    ).groupBy('age_group', 'year', 'country', 'province').agg(
+        F.count('*').alias('total_cases'),
+    )
---
+++ pipelines/silver_rape_bangladesh.py
@@ -0,0 +1,36 @@
+import pyspark.sql.functions as F
+import os
+
+STORAGE_ACCOUNT = os.getenv('STORAGE_ACCOUNT')
+STORAGE_ACCOUNT_KEY = os.getenv('STORAGE_ACCOUNT_KEY')
+
+@dlt.table(name='turkey_murder', comment='turkey murder data')
+def silver_turkey_murder():
+    spark.conf.set(STORAGE_ACCOUNT, STORAGE_ACCOUNT_KEY)
+    bangladesh_df = spark.read\
+                    .option("header", "true")\
+                    .option("inferSchema", "true")\
+                    .csv("abfss://datasets@tfmstorageacc.dfs.core.windows.net/rape_total_2001_2021-bangladesh.csv")
+    age_group_dict = {
+        'children':'child',
+        'women':'adult',
+        'Total_victims':'any'
+    }
+    return bangladesh_df.drop(
+        'Unidentified_age'
+    ).melt(
+        ids=['Years'],
+        values=['Total_victims', 'children', 'women'],
+        variableColumnName='age_group',
+        valueColumnName='total_cases',
+    ).replace(
+        age_group_dict,
+        subset=['age_group'],
+    ).withColumn(
+        'country', F.lit('bangladesh'),
+    ).select(
+        F.col('Years').alias('year'),
+        F.col('age_group'),
+        F.col('total_cases'),
+        F.col('country'),
+    )
