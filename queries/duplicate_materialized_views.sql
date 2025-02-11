c53c0dca7a6b2e6bb77f44e045a039a4e078ab9f
DustyQt <99213793+DustyQt@users.noreply.github.com>
2025-02-11 22:46:47 +0000

    Create duplicate_materialized_views.sql

---
+++ queries/duplicate_materialized_views.sql
@@ -0,0 +1,29 @@
+CREATE TABLE unity_catalog.violence_tables.gold_reported_cases_country AS
+SELECT * FROM unity_catalog.violence.gold_reported_cases_country;
+
+CREATE TABLE unity_catalog.violence_tables.silver_country AS
+SELECT * FROM unity_catalog.violence.silver_country;
+
+CREATE TABLE unity_catalog.violence_tables.silver_genital_mutilation AS
+SELECT * FROM unity_catalog.violence.silver_genital_mutilation;
+
+CREATE TABLE unity_catalog.violence_tables.silver_murder_turkey AS
+SELECT * FROM unity_catalog.violence.silver_murder_turkey;
+
+CREATE TABLE unity_catalog.violence_tables.silver_pacific_violence AS
+SELECT * FROM unity_catalog.violence.silver_pacific_violence;
+
+CREATE TABLE unity_catalog.violence_tables.silver_perception_global AS
+SELECT * FROM unity_catalog.violence.silver_perception_global;
+
+CREATE TABLE unity_catalog.violence_tables.silver_rape_bangladesh AS
+SELECT * FROM unity_catalog.violence.silver_rape_bangladesh;
+
+CREATE TABLE unity_catalog.violence_tables.silver_violence_colombia AS
+SELECT * FROM unity_catalog.violence.silver_violence_colombia;
+
+CREATE TABLE unity_catalog.violence_tables.silver_violence_india AS
+SELECT * FROM unity_catalog.violence.silver_violence_india;
+
+CREATE TABLE unity_catalog.violence_tables.silver_violence_malaysia AS
+SELECT * FROM unity_catalog.violence.silver_violence_malaysia;
